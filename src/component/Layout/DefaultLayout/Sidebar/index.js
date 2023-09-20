249164205a4df9938643d642cc6b51dacacaa68b
DeathLotu5 <duongph.4a@gmail.com>
2023-09-20 07:01:01 +0700

    first commit

---
+++ src/component/Layout/DefaultLayout/Sidebar/index.js
@@ -0,0 +1,13 @@
+import classnames from 'classnames/bind';
+import styles from './Sidebar.module.scss'
+
+const cx = classnames.bind(styles)
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side Bar</h2>
+        </aside>
+    );
+}
+
+export default Sidebar;
---
+++ src/component/Layout/DefaultLayout/index.js
@@ -0,0 +1,23 @@
+import classnames from 'classnames/bind';
+import Header from '../components/Header';
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classnames.bind(styles);
+
+function DefaultLayout( {children} ) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+
+                <div className={cx('content')}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
